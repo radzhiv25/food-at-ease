ce7f13ffcb5c2972d7ef2d9bc97eae953fc8411a
radzhiv <radzhivkrishna@gmail.com>
2024-05-23 18:21:49 +0530

    Initalized React App with TailwindCSS ⚒

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <div className="">
+    <h1 className='text-gray-500 p-2 text-center font-semibold'>Simplyfying food ordering at your car window.</h1>
+   </div>
+   </>
+  )
+}
+
+export default App
